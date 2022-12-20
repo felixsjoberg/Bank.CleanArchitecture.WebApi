92f0a7a48e836937944097cf25e2beaee5ba9c1f
Felix Sjöberg <felixsjoberg95@gmail.com>
2022-12-20 11:18:51 +0100

    Global error handling. Middleware,Filters,ProblemDetails

---
+++ BankApplication/Controllers/ErrorsController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankApplication.Api.Controllers
+{
+    public class ErrorsController : ControllerBase
+    {
+        [Route("/error")]
+        public IActionResult Error()
+        {
+            //accessing the thrown exception.
+            var exception = HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
+            return Problem(title: exception?.Message,statusCode: 400);
+        }
+    }
+}
--- BankApplication/Program.cs
+++ BankApplication/Program.cs
@@ -1,30 +1,30 @@
-﻿using BankApplication.Application;
-using BankApplication.Application.Authentication;
+﻿using BankApplication.Api.Filters;
+using BankApplication.Api.Middleware;
+using BankApplication.Application;
 using BankApplication.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddApplication()
                 .AddInfrastructure(builder.Configuration);
 
+//builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+//app.UseMiddleware<ErrorHandlingMiddleware>();
+app.UseExceptionHandler("/error");
 app.UseHttpsRedirection();
-
-app.UseAuthentication();
-app.UseAuthorization();
 
 app.UseRouting();
 app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+
 app.Run();
 
